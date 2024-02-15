f9ffc33d65b3f479849320800e411c875e6bc519
Shivam Rajput <shivam.rajput@thoughtworks.com>
2024-02-15 19:35:05 +0530

    Shivi | Add boxed container in hero banner text.

--- src/components/widgets/HeroBanner.tsx
+++ src/components/widgets/HeroBanner.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import type {WidgetPropType} from './widget'
 import {Stack, styled, Typography} from '@mui/material'
+import {BoxedContainer} from '../atoms'
 
 const Container = styled(Stack)<{src: string; bg: 'color' | 'image'}>(({theme, src, bg}) => ({
   background: bg === 'color' ? src : `url(${src})`,
   minHeight: theme.spacing(40),
   justifyContent: 'center',
   alignItems: 'center',
-  '&>h1': {
+  '& h1': {
     fontSize: theme.spacing(6),
     fontWeight: 500,
     [theme.breakpoints.up('md')]: {
@@ -26,7 +27,11 @@ type HeroBannerPropsType = {
 export const HeroBanner: React.FC<WidgetPropType<HeroBannerPropsType>> = ({data}) => {
   return (
     <Container bg={data.bg} src={(data.bg === 'color' ? data.color : data.image?.src) ?? 'white'}>
-      <Typography variant={'h1'}>{data.title}</Typography>
+      <BoxedContainer justifyContent={'center'} alignItems={'center'}>
+        <Typography variant={'h1'} textAlign={'center'}>
+          {data.title}
+        </Typography>
+      </BoxedContainer>
     </Container>
   )
 }
