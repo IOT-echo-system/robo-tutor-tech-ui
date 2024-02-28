d7dbd05091519fb78ed83cfe353fe36a61d5e461
Shivam Rajput <shivam.rajput@thoughtworks.com>
2024-02-28 10:22:29 +0530

    Shivi | Add backend api for contact us

--- src/components/widgets/ContactUs.tsx
+++ src/components/widgets/ContactUs.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import type {WidgetPropType} from './widget'
 import type {TextFieldProps} from '@mui/material'
 import {Button, Checkbox, Stack, TextField, Typography} from '@mui/material'
@@ -6,6 +6,7 @@ import {BoxedContainer} from '../atoms'
 import Link from 'next/link'
 import theme from '../../theme/theme'
 import {useForm} from '../../hooks'
+import WebClient from 'web-client-starter'
 
 type ContactUsPropsType = {
   title: string
@@ -14,6 +15,10 @@ type ContactUsPropsType = {
 export const ContactUs: React.FC<WidgetPropType<ContactUsPropsType>> = ({data}) => {
   const initialValue = {name: '', email: '', subject: '', message: '', consent: false}
   const {values, onChange, onSubmit} = useForm(initialValue)
+  const [submitted, setSubmitted] = useState(false)
+  const [responseMessage, setResponseMessage] = useState('')
+  const [error, setError] = useState(false)
+
   const fields: TextFieldProps[] = [
     {
       label: 'Full name',
@@ -52,7 +57,21 @@ export const ContactUs: React.FC<WidgetPropType<ContactUsPropsType>> = ({data}) 
   ]
 
   const handleSubmit = () => {
-    // console.log(formValues)
+    setError(false)
+    setResponseMessage('')
+    WebClient.post({
+      baseUrl: '/api',
+      path: '/contact',
+      body: values
+    })
+      .then(() => {
+        setSubmitted(true)
+        setResponseMessage('Thank you contacting us! Our team will reach out to you soon.')
+      })
+      .catch(() => {
+        setError(true)
+        setResponseMessage('Something went wrong, please try again!')
+      })
   }
 
   return (
@@ -62,49 +81,52 @@ export const ContactUs: React.FC<WidgetPropType<ContactUsPropsType>> = ({data}) 
           <Typography variant={'h4'} component={'h2'}>
             {data.title}
           </Typography>
+          {responseMessage && <Typography color={error ? 'error' : 'success'}>{responseMessage}</Typography>}
         </Stack>
-        <Stack component={'form'} spacing={2} onSubmit={onSubmit(handleSubmit)}>
-          {fields.map((props, index) => {
-            return <TextField key={`text-field-${index}`} {...props} />
-          })}
-          <Stack
-            direction={'row'}
-            alignItems={'start'}
-            spacing={1}
-            p={1}
-            sx={{background: theme.palette.background.default}}
-          >
-            <Checkbox
-              sx={{padding: 0}}
-              value={values.consent}
-              onChange={event => {
-                onChange('consent', event.target.checked)
-              }}
-              required
-            />
-            <Typography variant={'body2'}>
-              I agree to share my information with you and understand it will be used as described in{' '}
-              <Typography
-                component={Link}
-                href={'/privacy-policy'}
-                sx={{fontSize: 'inherit', textDecoration: 'underline'}}
-              >
-                Robotutor Tech's privacy policy
-              </Typography>
-              .
-            </Typography>
-          </Stack>
-          <Stack direction={'row'}>
-            <Button
-              size={'large'}
-              type={'submit'}
-              variant={'contained'}
-              sx={{padding: '8px 64px', textTransform: 'initial'}}
+        {!submitted && (
+          <Stack component={'form'} spacing={2} onSubmit={onSubmit(handleSubmit)}>
+            {fields.map((props, index) => {
+              return <TextField key={`text-field-${index}`} {...props} />
+            })}
+            <Stack
+              direction={'row'}
+              alignItems={'start'}
+              spacing={1}
+              p={1}
+              sx={{background: theme.palette.background.default}}
             >
-              Submit
-            </Button>
+              <Checkbox
+                sx={{padding: 0}}
+                value={values.consent}
+                onChange={event => {
+                  onChange('consent', event.target.checked)
+                }}
+                required
+              />
+              <Typography variant={'body2'}>
+                I agree to share my information with you and understand it will be used as described in{' '}
+                <Typography
+                  component={Link}
+                  href={'/privacy-policy'}
+                  sx={{fontSize: 'inherit', textDecoration: 'underline'}}
+                >
+                  Robotutor Tech's privacy policy
+                </Typography>
+                .
+              </Typography>
+            </Stack>
+            <Stack direction={'row'}>
+              <Button
+                size={'large'}
+                type={'submit'}
+                variant={'contained'}
+                sx={{padding: '8px 64px', textTransform: 'initial'}}
+              >
+                Submit
+              </Button>
+            </Stack>
           </Stack>
-        </Stack>
+        )}
       </Stack>
     </BoxedContainer>
   )
