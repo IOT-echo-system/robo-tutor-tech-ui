58d5266c5a8dca733e3def02d63d37798a7cf65f
Shivam Rajput <raj.shiviraj@gmail.com>
2024-06-16 19:27:31 +0530

    Shivi | Add public url.

--- src/config/cmsApiConfig.ts
+++ src/config/cmsApiConfig.ts
@@ -1,5 +1,6 @@
+export const PUBLIC_URL = 'https://www.robotutortech.com'
 export const cmsApiConfig = {
-  baseUrl: '/api',
+  baseUrl: `${PUBLIC_URL}/api`,
   siteInfo: '/site-info',
   mainMenu: '/main-menu?populate=navLink',
   footer: '/footer?populate=sections.navLinks,social.socials'
