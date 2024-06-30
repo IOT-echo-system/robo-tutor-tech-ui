dbfd82320f9d4ca982fd99c72efdea1d80112e38
Shivam Rajput <raj.shiviraj@gmail.com>
2024-06-30 22:34:25 +0530

    Shivi | remove env variable.

--- src/config/cmsApiConfig.ts
+++ src/config/cmsApiConfig.ts
@@ -1,10 +1,8 @@
 // eslint-disable-next-line no-process-env
 const API_BASE_URL = process.env.API_BASE_URL ?? ''
-// eslint-disable-next-line no-process-env
-const API_BASE_ASSETS_URL = process.env.API_BASE_ASSETS_URL ?? ''
 export const cmsApiConfig = {
   baseUrl: `${API_BASE_URL}/api`,
-  assets: `${API_BASE_ASSETS_URL}/assets`,
+  assets: 'https://iot-echo-system.github.io/assets',
   siteInfo: '/site-info?populate[seo][populate][0]=metaImage',
   mainMenu: '/main-menu?populate=navLink',
   footer: '/footer?populate=sections.navLinks,social.socials',
