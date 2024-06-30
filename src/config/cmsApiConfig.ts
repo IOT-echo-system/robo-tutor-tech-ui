1c8f9091bcf11a14fd9eccd6f2cc29f99b0a264e
Shivam Rajput <raj.shiviraj@gmail.com>
2024-06-30 22:08:34 +0530

    Shivi | add env variable.

--- src/config/cmsApiConfig.ts
+++ src/config/cmsApiConfig.ts
@@ -1,8 +1,10 @@
 // eslint-disable-next-line no-process-env
 const API_BASE_URL = process.env.API_BASE_URL ?? ''
+// eslint-disable-next-line no-process-env
+const API_BASE_ASSETS_URL = process.env.API_BASE_ASSETS_URL ?? ''
 export const cmsApiConfig = {
   baseUrl: `${API_BASE_URL}/api`,
-  assets: `${API_BASE_URL}/assets`,
+  assets: `${API_BASE_ASSETS_URL}/assets`,
   siteInfo: '/site-info?populate[seo][populate][0]=metaImage',
   mainMenu: '/main-menu?populate=navLink',
   footer: '/footer?populate=sections.navLinks,social.socials',
